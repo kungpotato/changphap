87f59221b1acdb8a46e532d5b3d9aaa74d75e52e
Kiitsak Pharaphan <kungpotato2008@hotmail.com>
2019-06-27 12:24:02 +0700

    style

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>kungpotato</h1>
+      <h2>Hello</h2>
+      <style jsx>{`
+      h1{
+        color: #4287f5;
+      }
+      h2{
+        color: #43cc31;
+      }
+    `}</style>
+    </div>
+  )
+}
+
+export default Home
