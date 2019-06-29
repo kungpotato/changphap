bb7d9e61bcd3ee368b1478a0630889aaf5558a61
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-06-29 19:24:42 +0700

    create side menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,46 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 // import Typography from '@material-ui/core/Typography'
 import Link from 'next/link'
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles(() => ({
-  menuButton: {
-    marginLeft: 'auto',
-  },
-}))
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import useWindowSize from '../custom-hooks/useWindowSize'
 
 const Header = () => {
-  const classes = useStyles()
+  const [isShow, setIsShow] = useState(true)
+  const [anchorEl, setAnchorEl] = useState(null)
+  let windowSize = useWindowSize()
+
+  useEffect(() => {
+    if (windowSize.width < 700) {
+      setIsShow(false)
+    } else {
+      setIsShow(true)
+    }
+  }, [windowSize])
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget)
+  }
+
+  function handleClose() {
+    setAnchorEl(null)
+  }
 
   return (
-    <AppBar
-      position="static"
-      color="default"
-      style={{ background: 'rgba(0,0,0,0.5)' }}
-    >
-      <Toolbar>
-        <Link href="/">
-          <img
-            src="./static/images/logo1.png"
-            alt=""
-            style={{ width: '150px', cursor: 'pointer' }}
-          />
-        </Link>
-        <div className={classes.menuButton}>
+    <React.Fragment>
+      <AppBar
+        position="static"
+        color="default"
+        style={{ background: 'rgba(0,0,0,0.5)' }}
+      >
+        <Toolbar>
           <Link href="/">
-            <div className="menu">บทความ</div>
+            <img
+              src="./static/images/logo1.png"
+              alt=""
+              style={{ width: '150px', cursor: 'pointer' }}
+            />
           </Link>
+
+          {isShow ? (
+            <div style={{ marginLeft: 'auto' }}>
+              <Link href="/">
+                <div className="menu">บทความ</div>
+              </Link>
+              <Link href="/">
+                <div className="menu">สมัครช่างภาพ</div>
+              </Link>
+              <Link href="/">
+                <div className="menu">เข้าสู่ระบบ</div>
+              </Link>
+            </div>
+          ) : (
+            <div style={{ marginLeft: 'auto' }}>
+              <IconButton onClick={handleClick}>
+                <i className="material-icons menu">menu</i>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
           <Link href="/">
-            <div className="menu">สมัครช่าวภาพ</div>
+            <div className="side-menu">บทความ</div>
           </Link>
-          <Link href="/" style={{ float: 'rigth' }}>
-            <div className="menu">เข้าสู่ระบบ</div>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="/">
+            <div className="side-menu">สมัครช่างภาพ</div>
           </Link>
-        </div>
-      </Toolbar>
-    </AppBar>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="/">
+            <div className="side-menu">เข้าสู่ระบบ</div>
+          </Link>
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
   )
 }
 
---
+++ custom-hooks/useWindowSize.js
@@ -0,0 +1,32 @@
+/*global window */
+import { useEffect, useState, useCallback } from 'react'
+
+const useWindowSize = () => {
+  const isClient = typeof window === 'object'
+
+  const getSize = useCallback(() => {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    }
+  }, [isClient])
+
+  const [windowSize, setWindowSize] = useState(getSize)
+
+  useEffect(() => {
+    if (!isClient) {
+      return false
+    }
+
+    const handleResize = () => {
+      setWindowSize(getSize())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [getSize, isClient]) // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize
+}
+
+export default useWindowSize
