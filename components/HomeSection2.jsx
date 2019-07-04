48c59833f7526a2523d9562dcb2069ddb0dd78d4
Kiitsak Pharaphan <kungpotato2008@hotmail.com>
2019-07-04 16:33:17 +0700

    fix

--- components/HomeSection2.jsx
+++ components/HomeSection2.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect } from 'react'
 import { withRouter } from 'next/router'
-<<<<<<< HEAD
 import Howto from './Howto'
 import LastProfile from './LastProfile'
-=======
-import Howto from '../components/Howto'
-import LastProfile from '../components/LastProfile'
->>>>>>> ef20e82d74bcc18aae5f903e7a3dd2d01cd5a1a5
 
 const HomeSec2 = () => {
   useEffect(() => {}, [])
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import SearchBox from '../components/SearchBox'
-<<<<<<< HEAD
 import HomeSec2 from '../components/HomeSection2'
-=======
-import HomeSec2 from './HomeSection2'
-import '../styles/styles.less'
->>>>>>> ef20e82d74bcc18aae5f903e7a3dd2d01cd5a1a5
 
 const Home = () => {
   return (
