4381862225f06506a6200dcc8e84c1864342b1e1
Kiitsak Pharaphan <kungpotato2008@hotmail.com>
2019-07-04 15:51:16 +0700

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
 import Header from '../components/Header'
 import SearchBox from '../components/SearchBox'
 import HomeSec2 from './HomeSection2'
->>>>>>> done section2
 import '../styles/styles.less'
 // import { getsUserFromGoogle, getsUserFromFacebook } from '../auth/authen'
-import Header from '../components/Header'
 
 const Home = () => {
   // const handleLoginGoogle = async () => {
@@ -20,36 +16,11 @@ const Home = () => {
   // }
 
   return (
-    <div>
+    <React.Fragment>
       <Header />
-<<<<<<< HEAD
-      <div className="root">
-        <div
-          style={{
-            width: '100%',
-            position: 'absolute',
-            top: '0',
-            zIndex: '-1',
-            height: '70%',
-            overflow: 'hidden',
-          }}
-        >
-          <img
-            src="./static/images/photography.jpg"
-            alt=""
-            style={{
-              width: '100%',
-              position: 'relative',
-            }}
-          />
-        </div>
-      </div>
-    </div>
-=======
       <SearchBox />
-      <HomeSec2/>
+      <HomeSec2 />
     </React.Fragment>
->>>>>>> done section2
   )
 }
 
