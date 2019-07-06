78ef76cd3eb6c34ad6f5622d4ffa7213e1883cbf
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-07-06 10:46:01 +0700

    adjust height

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -43,7 +43,7 @@ const Profile = () => {
         <div
           style={{
             width: '100%',
-            height: '50%',
+            height: '45%',
             position: 'absolute',
             top: '0',
             zIndex: '-1',
