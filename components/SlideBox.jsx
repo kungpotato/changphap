17ee88d88fc932ce993f657844e589346127d9c3
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-07-07 11:52:42 +0700

    carosel

--- components/SlideBox.jsx
+++ components/SlideBox.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
 
 const SlideBox = () => {
-  return <div>test</div>
+  const handleOnDragStart = e => e.preventDefault()
+  return (
+    <div>
+      <AliceCarousel
+        mouseDragEnabled
+        responsive={{
+          0: {
+            items: 1,
+          },
+          1024: {
+            items: 3,
+          },
+        }}
+      >
+        <img
+          src='./static/images/bride.jpg'
+          onDragStart={handleOnDragStart}
+          className='yours-custom-class'
+        />
+        <img
+          src='./static/images/bride.jpg'
+          onDragStart={handleOnDragStart}
+          className='yours-custom-class'
+        />
+        <img
+          src='./static/images/bride.jpg'
+          onDragStart={handleOnDragStart}
+          className='yours-custom-class'
+        />
+        <img
+          src='./static/images/bride.jpg'
+          onDragStart={handleOnDragStart}
+          className='yours-custom-class'
+        />
+        <img
+          src='./static/images/bride.jpg'
+          onDragStart={handleOnDragStart}
+          className='yours-custom-class'
+        />
+      </AliceCarousel>
+    </div>
+  )
 }
 
 export default SlideBox
--- pages/register.jsx
+++ pages/register.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SlideBox from '../components/SlideBox'
 
 const Register = () => {
   return (
     <div>
       <Header />
+      <SlideBox />
     </div>
   )
 }
